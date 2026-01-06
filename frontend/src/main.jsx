abe633f1f8fd6bc0d7a44bda41dd9d12365b7c59
Giacomo Favale <114728011+gfavale1@users.noreply.github.com>
2026-01-06 12:34:21 +0100

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,6 @@ import "./styles/index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { MsalProvider } from "@azure/msal-react";
 
 import App from "./App";
 import { msalInstance } from "./auth/msal";
